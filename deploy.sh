afbfa6ae0600e888ed7b17f3f4c9a1d331344912
Otmar Braunecker <otmarvonwien@hotmail.com>
2019-09-25 18:17:32 +0200

    fixed typo in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -11,6 +11,6 @@ docker push otmarvonwien/multi-server:$SHA
 docker push otmarvonwien/multi-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deplyoments/server-deployment server=otmarvonwien/multi-server:$SHA
-kubectl set image deplyoments/client-deployment client=otmarvonwien/multi-client:$SHA
-kubectl set image deplyoments/worker-deployment worker=otmarvonwien/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=otmarvonwien/multi-server:$SHA
+kubectl set image deployments/client-deployment client=otmarvonwien/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=otmarvonwien/multi-worker:$SHA
